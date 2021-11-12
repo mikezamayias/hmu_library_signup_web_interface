40c85e83786cbff6eb13a7bd2fd03eabaccb8233
Mike Zamayias <mzamagias@gmail.com>
2021-11-12 22:19:37 +0200

    working on a date picker

---
+++ lib/src/widgets/yellow_date_picker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class YellowDatePicker extends StatefulWidget {
+  const YellowDatePicker({Key? key}) : super(key: key);
+
+  @override
+  _YellowDatePickerState createState() => _YellowDatePickerState();
+}
+
+class _YellowDatePickerState extends State<YellowDatePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
