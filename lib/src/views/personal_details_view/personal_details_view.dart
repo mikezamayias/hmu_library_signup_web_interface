5efc34106508b0c8e777b4940d688471c59a9179
Mike Zamayias <mzamagias@gmail.com>
2021-11-12 22:19:28 +0200

    doing the work

--- lib/src/views/personal_details_view/personal_details_view.dart
+++ lib/src/views/personal_details_view/personal_details_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/navigation_button_bar.dart';
+import '../../widgets/yellow_text_form_field.dart';
 import '../default_view/default_view.dart';
 
 class PersonalDetailsView extends StatefulWidget {
@@ -10,11 +12,53 @@ class PersonalDetailsView extends StatefulWidget {
 }
 
 class _PersonalDetailsViewState extends State<PersonalDetailsView> {
+  TextEditingController firstNameTextController = TextEditingController();
+  TextEditingController lastNameTextController = TextEditingController();
+  TextEditingController phoneNumberTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const DefaultView(
-      headerChildren: [],
-      contentChildren: [],
+    return DefaultView(
+      headerChildren: const [
+        Center(
+          child: Text(
+            'Fill in your\npersonal details',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 42,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+      contentChildren: [
+        const Spacer(
+          flex: 9,
+        ),
+        YellowTextFormField(
+          hintText: 'Enter your first name',
+          labelText: 'First Name',
+          textEditingController: firstNameTextController,
+        ),
+        YellowTextFormField(
+          hintText: 'Enter your last name',
+          labelText: 'Last Name',
+          textEditingController: lastNameTextController,
+          isPassword: true,
+        ),
+        YellowTextFormField(
+          hintText: 'Enter your phone number',
+          labelText: 'Phone Number',
+          textEditingController: phoneNumberTextController,
+        ),
+        const Spacer(
+          flex: 9,
+        ),
+        const NavigationButtonBar(),
+        const Spacer(
+          flex: 9,
+        ),
+      ],
     );
   }
 }
