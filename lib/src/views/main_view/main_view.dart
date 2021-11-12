2c8551f18f8e22856e71850024581e29d0721121
Mike Zamayias <mzamagias@gmail.com>
2021-11-12 15:25:45 +0200

    Building the ui
    Added ui components

--- lib/src/views/main_view/main_view.dart
+++ lib/src/views/main_view/main_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hmu_library_signup_web_interface/src/widgets/secondary_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../widgets/main_button.dart';
 import '../default_view/default_view.dart';
@@ -11,9 +13,11 @@ class MainView extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultView(
       headerChildren: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(102, 102, 102, 0),
-          child: Expanded(child: Image.asset('assets/images/hmu_logo.png')),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(102, 102, 102, 0),
+            child: Image.asset('assets/images/hmu_logo.png'),
+          ),
         ),
         Padding(
           padding: const EdgeInsets.fromLTRB(90, 75, 90, 90),
@@ -29,15 +33,85 @@ class MainView extends StatelessWidget {
         ),
       ],
       contentChildren: [
+        const Spacer(flex: 9),
+        const Text(
+          'Sign up',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const Spacer(flex: 9),
+        const Text(
+          'Continue with',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const Spacer(flex: 1),
         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            const Spacer(flex: 9),
             MainButton(
-              onPressed: () => debugPrint('Sign in with Google'),
+              onPressed: () => debugPrint('give me your email'),
+              string: 'email',
+            ),
+            const Spacer(flex: 1),
+            MainButton(
+              onPressed: () => launch(
+                  'https://accounts.google.com/signin/v2/identifier?hl=el&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin'),
               string: 'Google',
             ),
+            const Spacer(flex: 1),
+            MainButton(
+              onPressed: () => launch('https://www.linkedin.com/login'),
+              string: 'LinkedIn',
+            ),
+            const Spacer(flex: 9),
           ],
-        )
+        ),
+        const Spacer(flex: 1),
+        RichText(
+          text: const TextSpan(
+            children: [
+              TextSpan(
+                text: 'I agree to ',
+              ),
+              TextSpan(
+                text: 'Terms of Service',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: ' and ',
+              ),
+              TextSpan(
+                text: 'Privacy Policy',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: '.',
+              ),
+            ],
+          ),
+        ),
+        const Spacer(flex: 9),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const <Widget>[
+            SecondaryButton(string: 'Sign in'),
+          ],
+        ),
+        const Spacer(flex: 9),
       ],
     );
   }
